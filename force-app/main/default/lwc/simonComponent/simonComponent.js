b9ed3446dc89244efa1ca370a42ae9710eadf955
Evan <evan.devizio@gmail.com>
2021-09-16 01:05:46 -0400

    integrated Jacob's implementation for sound. added random sequences and play button.

--- force-app/main/default/lwc/simonComponent/simonComponent.js
+++ force-app/main/default/lwc/simonComponent/simonComponent.js
@@ -1,3 +1,75 @@
 import { LightningElement } from 'lwc';
+import { redSound, greenSound, blueSound, yellowSound } from './sounds.js';
 
-export default class SimonComponent extends LightningElement {}
+export default class SimonComponent extends LightningElement {
+
+    turnCount; // keeps track of the current turn number
+    randomList = []; // random sequence generated for the round
+
+    connectedCallback(){
+        for(let i = 0; i < 10; i++){
+            // create a random sequence of ints, size 10, choices 0-3.
+            // 0: green, 1: red, 2: yellow, 3: blue
+            this.randomList.push(this.getRandomInt(4));
+        }
+        console.log('the sequence is: ' + this.randomList);
+        this.turnCount = 1;
+    }
+
+    playSequence(){
+        // play back the random sequence up to the current turn number
+        this.randomList.forEach(function(i){
+            switch(i){
+                case 0:
+                    greenSound();
+                    document.querySelector(".greenQuad").style.backgroundColor = "#00FF00";
+                    break;
+                case 1:
+                    redSound();
+                    document.querySelector(".redQuad").style.backgroundColor = "#FF0000";
+                    break;
+                case 2:
+                    yellowSound();
+                    document.querySelector(".yellowQuad").style.backgroundColor = "#FFFF00";
+                    break;
+                case 3:
+                    blueSound();
+                    document.querySelector(".blueQuad").style.backgroundColor = "#0000FF";
+                    break;
+            }
+        })
+        setTimeout(function(){
+            console.log('turn is over.');
+            this.turnCount++;
+        }, 500);
+    }
+
+    quadClickHandler(event){
+        let buttonClass = event.target.className;
+
+        switch(buttonClass){
+            case 'redQuad':
+                redSound();
+                break;
+            case 'blueQuad':
+                blueSound();
+                break;
+            case 'greenQuad':
+                greenSound();
+                break;
+            case 'yellowQuad':
+                yellowSound();
+                break;
+        }
+        console.log("clicked " + buttonClass);
+    }
+
+    playBtnClickHandler(){
+        this.playSequence();
+    }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+}
---
+++ force-app/main/default/lwc/simonComponent/sounds.js
@@ -0,0 +1,65 @@
+// FILE FOR SIMON SOUNDS
+//import functions in a file that you wish to use them in
+
+/*
+ * INSTRUCTIONS------------------!!!
+ *
+ * Step1: Upload all mp3 files as Static Resources.
+ * the names must be as follows: 
+ * red
+ * blue
+ * green
+ * yellow
+ * 
+ * Step2: Include below import in file.
+ * Make sure sounds.js is in the same directory as the component.
+ * 
+ * //include me!!!
+ * import { redSound, greenSound, blueSound, yellowSound } from './sounds.js';
+ * 
+ * Step3: Can utilize functions after import ex:
+ * 
+ * yellowSound();
+ * console.log('playing blue');
+ * blueSound();
+*/
+
+//-----------------------------------------
+
+//import synth1 from '@salesforce/resourceUrl/synth1';
+import red from '@salesforce/resourceUrl/red';
+import blue from '@salesforce/resourceUrl/blue';
+import green from '@salesforce/resourceUrl/green';
+import yellow from '@salesforce/resourceUrl/yellow';
+
+function beep(){
+    //var beepsound = new Audio(synth1);
+    //beepsound.play();
+    console.log('beep ');
+}
+
+function redSound(){
+    var beepsound = new Audio(red);
+    beepsound.volume = 0.3;
+    beepsound.play();
+}
+
+function blueSound(){
+    var beepsound = new Audio(blue);
+    beepsound.volume = 0.3;
+    beepsound.play();
+}
+
+function yellowSound(){
+    var beepsound = new Audio(yellow);
+    beepsound.volume = 0.3;
+    beepsound.play();
+}
+
+function greenSound(){
+    var beepsound = new Audio(green);
+    beepsound.volume = 0.3;
+    beepsound.play();
+}
+
+export{ redSound, blueSound, yellowSound, greenSound }
